82f2600e9c90299f1023d747b04d86dfab527c76
ChefDK User <chef@ip-172-31-15-168.ec2.internal>
2016-10-11 20:28:18 +0000

    Adding dynamic cpu properties

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -3,5 +3,14 @@ package 'tree' do
 end
 
 file '/etc/motd' do
-  content 'Property of jake'
+  content "Property of jake
+  IPADDRESS : #{node['ipaddress']}
+  HOSTNAME  : #{node['hostname']}
+  MEMORY    : #{node['memory']['total']}
+  CPU       : #{node['cpu']['0']['mhz']}
+"
+  mode '0644'
+  owner 'root'
+  group 'root'
 end
+
